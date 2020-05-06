25e667d9733b87d99deef97713bdf187084e92b8
carpenterzoe <39980577+carpenterzoe@users.noreply.github.com>
2020-05-06 11:28:26 +0800

    Update map提取对象数组中的有效值.js

--- map提取对象数组中的有效值.js
+++ map提取对象数组中的有效值.js
@@ -24,12 +24,12 @@ console.log(titleArr, 'titleArr') // [ '第一个标题', '第二个标题', '�
 // 数组中提取多个有效字段 （注意写法
 let res = arr.map( ({id, msg}) => ({ id, msg }) )
 
-console.log(res,' res') // [ { id: 1, msg: '哈哈哈' }, { id: 2, msg: '呼呼' }, { id: 3, msg: '吱吱吱' } ] ' res'
+console.log(res, 'res') // [ { id: 1, msg: '哈哈哈' }, { id: 2, msg: '呼呼' }, { id: 3, msg: '吱吱吱' } ] 'res'
 
 /*
   可能会出现如下错误写法
 
-  error 1 箭头函数的参数如果是item，可以不用()扩起来。如果是结构的对象，则需要()
+  error 1 箭头函数的参数如果是item，可以不用()扩起来。如果是解构的对象，则需要()
   let res1 = arr.map( {id, msg} => { id, msg } )
 
   error 2 箭头函数右侧，如果{id, msg} 整体是一个对象返回值，也需要()括起来。否则会被解析为花括号代码块。
