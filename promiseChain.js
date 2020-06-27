300eb46b87141791f7e998a7149262b33905d327
carpenterzoe <39980577+carpenterzoe@users.noreply.github.com>
2020-06-27 15:11:48 +0800

    Create promiseChain.js

---
+++ promiseChain.js
@@ -0,0 +1,29 @@
+// 遍历数组，叠加promise.then 链式调用
+
+let chain = [
+  {
+    resolved: (res) => console.log(res, 'resolved  111'),
+    rejected: () => console.log('rejected 111')
+  },
+  {
+    resolved: () => console.log('resolved  222'),
+    rejected: () => console.log('rejected 222')
+  },
+  {
+    resolved: () => console.log('resolved  333'),
+    rejected: () => console.log('rejected 333')
+  },
+  {
+    resolved: () => console.log('resolved  444'),
+    rejected: () => console.log('rejected 444')
+  }
+]
+
+let config = 'I am axios config'
+let promise = Promise.resolve(config)
+
+while (chain.length) {
+  const { resolved, rejected } = chain.shift()
+
+  promise = promise.then( resolved, rejected )
+}
